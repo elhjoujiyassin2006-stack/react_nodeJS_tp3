17e6cf47ed3aae1af2de5e9e3d4095683e0a7545
Med yassine <elhjoujiyassin2006@gmail.com>
2026-03-02 11:17:05 +0000

    kkk

--- src/components/MembersTable.jsx
+++ src/components/MembersTable.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/v1/members';
+const API_URL = 'https://node-js-tp3.vercel.app/api/v1/members';
 
 function MembersTable() {
     const [members, setMembers] = useState([]);
